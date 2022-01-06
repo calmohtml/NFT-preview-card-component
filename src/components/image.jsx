52e06513cfa898e38845daa2465d727cd97f8c4a
calmohtml <contactosegovia@protonmail.com>
2022-01-06 17:21:02 -0300

    hover added

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -2,21 +2,37 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import {
   equilibrium__container,
-  equilibrium__bg,
+  equilibrium__middle,
 } from "../styles/image.module.css";
 
 const Image = () => {
   return (
     <div className={equilibrium__container}>
-      <div className={equilibrium__bg}>
+      <StaticImage
+        src="../images/image-equilibrium.jpg"
+        alt="Image of NFT Equilibrium"
+        placeholder="blurred"
+      />
+      <div className={equilibrium__middle}>
         <StaticImage
-          src="../images/image-equilibrium.jpg"
-          alt="Image of NFT Equilibrium"
-          placeholder="blurred"
+          src="../images/icon-view.svg"
+          alt="Image of View"
+          placeholder="tracedSVG"
         />
       </div>
     </div>
   );
 };
+
+{
+  /* 
+<div class="container">
+  <img src="img_avatar.png" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">John Doe</div>
+  </div>
+</div> 
+*/
+}
 
 export default Image;
